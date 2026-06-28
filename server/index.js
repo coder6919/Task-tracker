f2948a6d483b9e62df91ab21d3ffd2194c6f4860
Saifshr <ssaifssyed17@gmail.com>
2026-06-28 14:19:39 +0530

    Backend fixed issues

--- server/index.js
+++ server/index.js
@@ -10,6 +10,11 @@ import taskRoutes from './routes/taskRoutes.js';
 dotenv.config();
 connectDB();
 
+app.use(cors({
+  origin: process.env.CLIENT_URL || "http://localhost:5173",
+  credentials: true
+}));
+
 const app = express();
 app.use(cors());
 app.use(express.json());
