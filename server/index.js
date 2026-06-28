3d01a6cb92f1a703259de65163d39a9fa87c75c2
Saifshr <ssaifssyed17@gmail.com>
2026-06-28 14:22:01 +0530

    second backend issue

--- server/index.js
+++ server/index.js
@@ -10,14 +10,16 @@ import taskRoutes from './routes/taskRoutes.js';
 dotenv.config();
 connectDB();
 
-app.use(cors({
-  origin: process.env.CLIENT_URL || "http://localhost:5173",
-  credentials: true
-}));
+
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.use(cors({
+  origin: process.env.CLIENT_URL || "http://localhost:5173",
+  credentials: true
+}));
 
 // Routes
 app.use('/api/auth', authRoutes);
